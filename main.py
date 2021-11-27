a9558775b203b1f668d7afa42a212f5d540507a0
Katerina Solstad (katya sorok) <37941283+katerinaSo@users.noreply.github.com>
2021-11-27 00:41:40 +0000

    dataframe added

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import streamlit as st
+import pandas as pd
+data={
+	"series_1":[1,2,3,4,5],
+	"series_2":[10,20,30,40,50]
+}
+df=pd.DataFrame(data)
 st.title('first streamlit app')
 st.subheader('add subheader')
 st.write('''multiline \n
-text 
-goes here 
-''')
+            text 
+             goes here''')
+
+st.write(df)
